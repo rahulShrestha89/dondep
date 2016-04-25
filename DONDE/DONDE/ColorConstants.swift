a6e17e2795038e2a87ddd19746caa3af0d309d52
Rahul Shrestha <rahul.shrestha@selu.edu>
2016-04-25 17:38:19 -0500

    setup

---
+++ DONDE/DONDE/ColorConstants.swift
@@ -0,0 +1,48 @@
+//
+//  ColorConstants.swift
+//  DONDE
+//
+//  Created by Rahul Shrestha on 4/18/16.
+//  Copyright © 2016 SLU. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    class func dondeBeigeColor() -> UIColor {
+        return UIColor(red: 1, green: 232/255, blue: 202/255, alpha: 1.0)
+    }
+    
+    class func dondeGreenColor() -> UIColor {
+        return UIColor(red: 19/255, green: 152/255, blue: 138/255, alpha: 1.0)
+    }
+   
+    class func dondeBlueColor() -> UIColor {
+        return UIColor(red: 25/255, green: 118/255, blue: 210/255, alpha: 1.0)
+    }
+    
+    class func dondeGreenLightColor() -> UIColor {
+        return UIColor(red: 19/255, green: 152/255, blue: 138/255, alpha: 0.2)
+    }
+    
+    class func dondeRedColor() -> UIColor {
+        return UIColor(red: 230/255, green: 74/255, blue: 25/255, alpha: 1.0)
+    }
+    
+    class func dondeRedLightColor() -> UIColor {
+        return UIColor(red: 238/255, green: 103/255, blue: 100/255, alpha: 0.2)
+    }
+    
+    class func dondeBrownColor() -> UIColor {
+        return UIColor(red: 55/255, green: 31/255, blue: 31/255, alpha: 1.0)
+    }
+    
+    class func dondeYellowColor() -> UIColor {
+        return UIColor(red: 249/255, green: 168/255, blue: 37/255, alpha: 1.0)
+    }
+    
+    class func dondeWhiteColor() -> UIColor {
+        return UIColor(red: 236/255, green: 239/255, blue: 241/255, alpha: 1.0)
+    }
+}
---
+++ DONDE/DONDE/SignInViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  DONDE
+//
+//  Created by Rahul Shrestha on 4/18/16.
+//  Copyright © 2016 SLU. All rights reserved.
+//
+
+import UIKit
+import DigitsKit
+import Crashlytics
+
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var lookAroundButton: UIButton!
+    @IBOutlet weak var connectWithPhoneButton: UIButton!
+    
+    @IBAction func onConnectWithPhoneButtonTapped(sender: AnyObject) {
+        
+        // Create a Digits appearance with native colors.
+        let configuration = DGTAuthenticationConfiguration(accountFields: .DefaultOptionMask)
+        
+        configuration.appearance = DGTAppearance()
+        configuration.appearance.backgroundColor = UIColor.dondeWhiteColor()
+        configuration.appearance.accentColor = UIColor.dondeGreenColor()
+        
+        // Start the Digits authentication flow with the custom appearance.
+        Digits.sharedInstance().authenticateWithViewController(nil, configuration:configuration) { (session, error) in
+            if session != nil {
+                
+                // Tie crashes to a Digits user ID in Crashlytics.
+                Crashlytics.sharedInstance().setUserIdentifier(session.userID)
+                
+                
+            } else {
+               
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        decorateButton(connectWithPhoneButton, color: UIColor.dondeGreenColor())
+        decorateButton(lookAroundButton, color: UIColor.dondeBlueColor())
+
+    }
+    
+    @IBAction func onLookAroundButtonTapped(sender: AnyObject) {
+        
+    }
+    // MARK: Utilities
+    
+    private func decorateButton(button: UIButton, color: UIColor) {
+        // Draw the border around a button.
+        button.layer.masksToBounds = false
+        button.layer.borderColor = color.CGColor
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 6
+        button.backgroundColor = color
+    }
+
+}
+
